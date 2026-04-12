0bc87e39dfa7e970ceeb50b17a5dc71c96cb5262
sengokyu <sengokyu+gh@gmail.com>
2026-04-12 11:23:31 +0900

    Configure vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globals: true,
     include: ["./src/**/*.test.ts"],
+    environment: "node",
   },
 });
